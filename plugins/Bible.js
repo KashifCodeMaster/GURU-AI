a8538378d3ec60c9f4029be7b5471d80a1354893
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-01-14 14:21:29 +0500

    Update Bible.js

--- plugins/Bible.js
+++ plugins/Bible.js
@@ -1,15 +1,14 @@
 import fetch from 'node-fetch';
-import { translate } from '@vitalets/google-translate-api';
 
 const BASE_URL = 'https://bible-api.com';
 
-let bibleChapterHandler = async (m, { conn }) => {
+let bibleChapterHandler = async (m, { conn, usedPrefix }) => {
   try {
     // Extract the chapter number or name from the command text.
     let chapterInput = m.text.split(' ').slice(1).join('').trim();
 
     if (!chapterInput) {
-      throw new Error(`Please specify the chapter number or name. Example: -bible john 3:16`);
+      throw new Error(`Please specify the chapter number or name. Example: ${usedPrefix}bible john 3:16`);
     }
 
     // Encode the chapterInput to handle special characters
@@ -19,24 +18,18 @@ let bibleChapterHandler = async (m, { conn }) => {
     let chapterRes = await fetch(`${BASE_URL}/${chapterInput}`);
     
     if (!chapterRes.ok) {
-      throw new Error(`Please specify the chapter number or name. Example: -bible john 3:16`);
+      throw new Error(`Unable to retrieve information for the specified chapter. Please check your input. Example: ${usedPrefix}bible john 3:16`);
     }
 
     let chapterData = await chapterRes.json();
-
-    let translatedChapterHindi = await translate(chapterData.text, { to: 'hi', autoCorrect: true });
-
-    let translatedChapterEnglish = await translate(chapterData.text, { to: 'en', autoCorrect: true });
 
     let bibleChapter = `
 📖 *The Holy Bible*\n
 📜 *Chapter ${chapterData.reference}*\n
 Type: ${chapterData.translation_name}\n
 Number of verses: ${chapterData.verses.length}\n
-🔮 *Chapter Content (English):*\n
-${translatedChapterEnglish.text}\n
-🔮 *Chapter Content (Hindi):*\n
-${translatedChapterHindi.text}`;
+🔮 *Chapter Content:*\n
+${chapterData.text}`;
 
     m.reply(bibleChapter);
   } catch (error) {
