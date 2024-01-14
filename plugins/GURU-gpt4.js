31e93dbdbcec61166ebc9fbd61729fe6e81dfc93
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-01-14 15:40:13 +0500

    Update GURU-gpt4.js

--- plugins/GURU-gpt4.js
+++ plugins/GURU-gpt4.js
@@ -1,50 +1,52 @@
-
-import displayLoadingScreen from '../lib/loading.js'
-import fetch from 'node-fetch'
-import {delay} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch';
+import { delay } from '@whiskeysockets/baileys';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
-    if (!text) throw `uhm.. what do you want to say?`
-    m.react('🤖')
-    //await displayLoadingScreen(conn, m.chat)
+    if (!text) {
+      m.reply(`Well, I can't read minds yet. How about sharing your thoughts with me?`);
+      return;
+    }
 
-
+    m.react('🤖');
     const prompt = encodeURIComponent(text);
-    let apiurl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`
+    let apiUrl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`;
+    const result = await fetch(apiUrl);
 
-    const result = await fetch(apiurl);
+    if (!result.ok) {
+      throw new Error(`Received an error response from the server: ${result.status} - ${result.statusText}`);
+    }
+
     const response = await result.json();
-    console.log(response)
-    const textt = response.result.reply;
-    await typewriterEffect(conn,m, m.chat , textt);
-       
+    const generatedText = response.result.reply;
+    await typewriterEffect(conn, m, m.chat, generatedText);
   } catch (error) {
     console.error(error);
-    m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
+    m.reply(`Oops! Something went haywire. Blame it on the gremlins, not us.`);
+  }
+};
+
+handler.help = ['gpt4 <text>'];
+handler.tags = ['AI'];
+handler.command = /^(gpt4)$/i;
+
+export default handler;
+
+async function typewriterEffect(conn, quoted, from, text) {
+  let { key } = await conn.sendMessage(from, { text: 'Whirring gears, bubbling potions... crafting your response!' }, { quoted: quoted });
+
+  for (let i = 0; i < text.length; i++) {
+    const partialText = text.slice(0, i + 1);
+    await conn.relayMessage(from, {
+      protocolMessage: {
+        key: key,
+        type: 14,
+        editedMessage: {
+          conversation: partialText
+        }
+      }
+    }, {});
+
+    await delay(100); // Adjust the delay time (in milliseconds) as needed
   }
 }
-handler.help = ['gemini <text>']
-handler.tags = ['tools']
-handler.command = /^(gpt4)$/i
-
-export default handler
-
-async function typewriterEffect(conn, quoted ,from, text) {
-    let { key } = await conn.sendMessage(from, { text: 'Thinking...' } , {quoted:quoted})
-  
-    for (let i = 0; i < text.length; i++) {
-      const noobText = text.slice(0, i + 1);
-      await conn.relayMessage(from, {
-        protocolMessage: {
-          key: key,
-          type: 14,
-          editedMessage: {
-            conversation: noobText
-          }
-        }
-      }, {});
-   
-       await delay(100); // Adjust the delay time (in milliseconds) as needed
-    }
-  }
