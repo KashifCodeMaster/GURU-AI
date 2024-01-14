80ad17db9295e70ffcb6f119a839334a61031677
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-01-14 14:58:53 +0500

    Update GURU-gemini.js

--- plugins/GURU-gemini.js
+++ plugins/GURU-gemini.js
@@ -1,28 +1,37 @@
-import {GoogleGenerativeAI} from '@google/generative-ai'
-import displayLoadingScreen from '../lib/loading.js'
-const genAI = new GoogleGenerativeAI('AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8');
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
+const genAI = new GoogleGenerativeAI('AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8');
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
-    if (!text) throw `uhm.. what do you want to say?`
-    m.react('🤖')
-    await displayLoadingScreen(conn, m.chat)
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    if (!text) throw `Come on! Don't be shy. What do you want to talk about?`;
+    
+    m.react('🤖');
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
-    const prompt = text
+    const prompt = text;
 
     const result = await model.generateContent(prompt);
     const response = result.response;
-    const textt = response.text();
-    m.reply(textt)
+    const generatedText = response.text();
+
+    await conn.relayMessage(m.chat, {
+      protocolMessage: {
+        type: 14,
+        editedMessage: {
+          conversation: generatedText
+        }
+      }
+    }, {});
+
   } catch (error) {
     console.error(error);
-    m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
+    m.reply('Oops! Something went wrong. Let me try to gather my thoughts again.');
   }
-}
-handler.help = ['gemini <text>']
-handler.tags = ['tools']
-handler.command = /^(gemini)$/i
+};
 
-export default handler
+handler.help = ['gemini <text>'];
+handler.tags = ['tools'];
+handler.command = /^(gemini)$/i;
+
+export default handler;
