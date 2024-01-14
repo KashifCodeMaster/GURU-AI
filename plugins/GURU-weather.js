35c444acdf3e45b46b1e3bacca4259fccf6fad3b
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-01-14 22:32:17 +0500

    Update GURU-weather.js

--- plugins/GURU-weather.js
+++ plugins/GURU-weather.js
@@ -1,22 +1,44 @@
-import axios from "axios"
+import axios from 'axios';
+
 let handler = async (m, { args }) => {
-if (!args[0]) throw "*Give a place to search*"
-try {
-const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
-const res = await response
-const name = res.data.name
-const Country = res.data.sys.country
-const Weather = res.data.weather[0].description
-const Temperature = res.data.main.temp + "°C"
-const Minimum_Temperature = res.data.main.temp_min + "°C"
-const Maximum_Temperature = res.data.main.temp_max + "°C"
-const Humidity = res.data.main.humidity + "%"
-const Wind = res.data.wind.speed + "km/h"
-const wea = `「 📍 」PLACE: ${name}\n「 🗺️ 」COUNTRY: ${Country}\n「 🌤️ 」VIEW: ${Weather}\n「 🌡️ 」TEMPERATURE: ${Temperature}\n「 💠 」 MINIMUM TEMPERATURE: ${Minimum_Temperature}\n「 📛 」 MAXIMUM TEMPERATURE: ${Maximum_Temperature}\n「 💦 」HUMIDITY: ${Humidity}\n「 🌬️ 」 WINDSPEED: ${Wind}`
-m.reply(wea)
-} catch {
-return "*ERROR*"}}
-handler.help = ['weather *<place>*']
-handler.tags = ['tools']
-handler.command = /^(climate|weather)$/i
-export default handler
+    try {
+        if (!args[0]) {
+            m.react('🤦🏻‍♀️');
+            throw '*Oh dear, where do you want to know the weather? Please provide a place.*';
+        }
+
+        m.react('⌛');
+
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+            params: {
+                q: args.join(' '),
+                units: 'metric',
+                appid: '060a6bcfa19809c2cd4d97a212b19273'
+            }
+        });
+
+        const data = response.data;
+        const place = data.name;
+        const country = data.sys.country;
+        const weatherDescription = data.weather[0].description;
+        const temperature = `${data.main.temp}°C`;
+        const minTemperature = `${data.main.temp_min}°C`;
+        const maxTemperature = `${data.main.temp_max}°C`;
+        const humidity = `${data.main.humidity}%`;
+        const windSpeed = `${data.wind.speed}km/h`;
+
+        const weatherInfo = `🌍 *Place:* ${place}, ${country}\n🌦️ *Weather:* ${weatherDescription}\n🌡️ *Temperature:* ${temperature}\n💡 *Min Temperature:* ${minTemperature}\n🔥 *Max Temperature:* ${maxTemperature}\n💧 *Humidity:* ${humidity}\n💨 *Wind Speed:* ${windSpeed}`;
+
+        m.reply(weatherInfo);
+        m.react('🌤️');
+    } catch (error) {
+        console.error('Error in weather command:', error);
+        m.reply('*❌ Oops! There seems to be a glitch in the weather forecast. Please try again later.*');
+        m.react('🥺');
+    }
+};
+
+handler.help = ['weather *<place>*'];
+handler.tags = ['tools'];
+handler.command = /^(climate|weather)$/i;
+export default handler;
