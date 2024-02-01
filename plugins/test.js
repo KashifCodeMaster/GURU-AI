ecdde6d7589c378890e245297abe360c3ab0880f
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-02-01 22:30:25 +0500

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,12 +1,27 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
-let info = `*BOT ACTIVE*`
-await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
-      isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
-}
-handler.customPrefix = /^(tes|tess|test)$/i
-handler.command = new RegExp
+import fs from 'fs';
+import fetch from 'node-fetch';
 
-export default handler
+let handler = async (m, { conn, usedPrefix: _p }) => {
+    let img = "https://telegra.ph/file/241972dd211afe6239140.jpg";
+    let info = `🦊 *Silver Fox Status Report* 🔧\n\n🛠️ *Current Status:* Fully Operational and Ready for Action\n🌐 *Server Uptime:* 99.99% (because perfection takes effort)\n💼 *Tasks Completed:* Countless and Still Counting\n🔧 *Maintenance Status:* None, I'm a well-oiled machine\n\n_Please let me know if there's anything I can assist you with!_ 😄`;
+
+    await conn.reply(m.chat, info, m, {
+        contextInfo: {
+            mentionedJid: [m.sender],
+            forwardingScore: 256,
+            isForwarded: true,
+            externalAdReply: {
+                title: "Silver Fox",
+                body: "Snapshot of my current status.",
+                sourceUrl: "youtube.com",
+                thumbnail: await conn.getFile(img)
+            }
+        }
+    });
+}
+
+handler.customPrefix = /^(report|status|test)$/i;
+handler.command = new RegExp;
+
+export default handler;
+    
