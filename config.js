0ea906514f4666138eda45e6a98d01ac9d436ea4
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-05-20 18:37:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,6 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -25,20 +23,29 @@ for (let i = 0; i < ownerlist.length; i += 2) {
     global.owner.push(owner);
 }
 
-// global.pairingNumber = "" // put your bot number here
+//global.pairingNumber = "" //put your bot number here
 
 global.mods = ['923036631033']
 global.prems = ['923036631033', '923068872881']
 global.allowed = ['923036631033', '923068872881']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 
-global.APIs = { // API Prefix
+global.APIs = {
+  // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz',
   dzx: 'https://api.dhamzxploit.my.id',
@@ -50,23 +57,24 @@ global.APIs = { // API Prefix
   akuari2: 'https://apimu.my.id',
   nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
+  fgmods: 'https://api-fgmods.ddns.net',
 }
-global.APIKeys = { // APIKey Here
+global.APIKeys = {
+  // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
   'https://api.neoxr.my.id': `${keysneoxr}`,
   'https://violetics.pw': 'beta',
   'https://zenzapis.xyz': `${keysxxx}`,
-  'https://api-fgmods.ddns.net': 'fg-dylux'
+  'https://api-fgmods.ddns.net': 'fg-dylux',
 }
 
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'Silver'
-global.author = 'Fox'
+global.packname = 'Silver Fox'
+global.author = ''
 global.menuvid = 'https://i.imgur.com/GPBlmZo.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com\n'
 global.dygp = 'https://chat.whatsapp.com'
@@ -92,4 +100,3 @@ watchFile(file, () => {
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
-  
