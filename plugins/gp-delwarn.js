5e3724cebd40cf1f6b58798a7dcf86f4e57719c7
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-10-15 11:37:29 +0500

    Update gp-delwarn.js

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -1,28 +1,30 @@
+let handler = async (m, { conn, args, groupMetadata }) => {
+    let who;
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    else who = m.chat;
 
-let handler = async (m, { conn, args, groupMetadata}) => {
-        let who
-        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-        else who = m.chat
-        if (!who) throw `✳️ Tag or mention someone`
-        if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-       let warn = global.db.data.users[who].warn
-       if (warn > 0) {
-         global.db.data.users[who].warn -= 1
-         m.reply(`⚠️ *DELWARN*
-         
-▢ Warns: *-1*
-▢ Warns total: *${warn - 1}*`)
-         m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
-         } else if (warn == 0) {
-            m.reply('✳️ The user has no warning')
-        }
+    if (!who) throw `⚠️ *Oops!* You forgot to tag someone.`;
+    if (!(who in global.db.data.users)) throw `❌ *User not found* in the database.`;
 
+    let warn = global.db.data.users[who].warn;
+
+    if (warn > 0) {
+        global.db.data.users[who].warn -= 1;
+        m.reply(`
+🧹 *Warning Cleared!*
+
+👤 *User:* @${who.split`@`[0]}
+⚠️ *Warns Remaining:* ${warn - 1}`, null, { mentions: [who] });
+    } else if (warn == 0) {
+        m.reply(`📢 *All Clear!* This user has no warnings to remove.`);
+    }
 }
-handler.help = ['delwarn @user']
-handler.tags = ['group']
-handler.command = ['delwarn', 'unwarn'] 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
 
-export default handler
+handler.help = ['delwarn @user', 'unwarn @user'];
+handler.tags = ['group'];
+handler.command = ['delwarn', 'unwarn'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
