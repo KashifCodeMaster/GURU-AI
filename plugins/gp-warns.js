7969e40535215be75217cd414b216875a5b15159
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-10-15 11:38:14 +0500

    Update gp-warns.js

--- plugins/gp-warns.js
+++ plugins/gp-warns.js
@@ -1,19 +1,22 @@
+let handler = async (m, { conn, args, groupMetadata }) => {
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    if (!(who in global.db.data.users)) throw `❌ *User not found* in the database.`;
 
-let handler = async (m, { conn, args, groupMetadata}) => {
-       let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-       if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-       let warn = global.db.data.users[who].warn
-       let name = conn.getName(who)
-      m.reply(`
- *WARNINGS*
+    let warn = global.db.data.users[who].warn;
+    let name = conn.getName(who);
 
-▢ *Name :* ${name} 
-▢ *Warns :* ${warn}`)
+    m.reply(`
+📊 *Warning Status*
+
+👤 *User:* ${name}
+⚠️ *Warnings:* ${warn}/${global.maxwarn}
+
+P.S.: Remember, ${warn >= global.maxwarn ? 'you’re at the limit!' : 'you’ve still got some room for mistakes.'}`);
 }
 
-handler.help = ['warns']
-handler.tags = ['group']
-handler.command = ['warns'] 
-handler.group = true
+handler.help = ['warns'];
+handler.tags = ['group'];
+handler.command = ['warns'];
+handler.group = true;
 
-export default handler
+export default handler;
