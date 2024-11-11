c326abf7d141c5bb641ce55f90b689a68660a5ed
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2024-11-11 15:19:29 +0500

    Update main-botInfo.js

--- plugins/main-botInfo.js
+++ plugins/main-botInfo.js
@@ -2,20 +2,25 @@ import { cpus as _cpus, totalmem, freemem } from 'os'
 import util from 'util'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
+
 let format = sizeFormatter({
-  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  std: 'JEDEC', 
   decimalPlaces: 2,
   keepTrailingZeroes: false,
   render: (literal, symbol) => `${literal} ${symbol}B`,
 })
+
 let handler = async (m, { conn, usedPrefix, command }) => {
   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
+
   const used = process.memoryUsage()
+
   const cpus = _cpus().map(cpu => {
     cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
     return cpu
   })
+
   const cpu = cpus.reduce((last, cpu, _, { length }) => {
     last.total += cpu.total
     last.speed += cpu.speed / length
@@ -36,47 +41,45 @@ let handler = async (m, { conn, usedPrefix, command }) => {
       irq: 0
     }
   })
+
   let old = performance.now()
-  
+
   let neww = performance.now()
   let speed = neww - old
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-let user = global.db.data.users[who]
-  
-let infobt = `
-≡ *INFO BOT*
+
+  if (!(who in global.db.data.users)) throw `🔍 Oops, this user isn't even in my database. Guess they don't exist... or maybe they're just invisible?`
+
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './default-avatar.jpg')
+  let user = global.db.data.users[who]
+  let botName = global.botName || "Robot" // Assuming the bot name is stored in a global variable
+
+  let infobt = `
+*🤖 Greetings, human! Here's a quick snapshot of my mechanical existence...*
+
+≡ *INFO ${botName.toUpperCase()}* (That's me, in case you forgot!)
   
 *STATE*
-▢ *${groupsIn.length}* GROUP CHATS
-▢ *${groupsIn.length}* united groups
-▢ *${groupsIn.length - groupsIn.length}* abandoned groups
-▢ *${chats.length - groupsIn.length}* private chats
-▢ *${chats.length}* Total Chats
-
-*≡ OWNER*
-  *GURU*
-▢ Instagram :
-  • https://instagram.com/asli_guru69
-▢ GitHub :
-  • https://github.com/Guru322
-▢ Telegram : 
-  • t.me/i_want_to_be_isekaied (GURU) 
-▢ YouTube : 
-  • https://youtube.com/@Asliguru
-
- *≡ S E R V E R*
-*🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
-*🔵 FreeRAM:* ${format(freemem())}
-
-*≡  NodeJS memory *
+▢ *${groupsIn.length}* GROUP CHATS (Because who doesn’t love chaos?)
+▢ *${groupsIn.length}* united groups (Teamwork makes the dream work, apparently)
+▢ *${groupsIn.length - groupsIn.length}* abandoned groups (Don't worry, they’re not haunting us... yet)
+▢ *${chats.length - groupsIn.length}* private chats (A bit of peace... finally)
+▢ *${chats.length}* Total Chats (Can we just get a moment of silence for this number?)
+  
+*≡ SERVER STATUS*
+*🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())} (Just a bit more memory, please?)
+*🔵 FreeRAM:* ${format(freemem())} (No, I promise, this is not a "memory leak" situation...)
+  
+*≡ NODEJS MEMORY USAGE*
 ${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
-`
-conn.sendFile(m.chat, pp, 'prefil.jpg', infobt, m, false, { mentions: [who] })
-m.react(done)
 
+*System running smooth... for now.* 😎
+*Note: Yes, I am a robot. And no, I don't have feelings... yet.*
+`
+  conn.sendFile(m.chat, pp, 'profile.jpg', infobt, m, false, { mentions: [who] })
+  m.react('✅')
 }
+
 handler.help = ['info']
 handler.tags = ['main']
 handler.command = ['info', 'infobot', 'botinfo']
