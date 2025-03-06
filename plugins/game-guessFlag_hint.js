edddc2af7e084f026ae2e4dbfc06784f75796a40
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2025-03-06 23:35:29 +0500

    Update game-guessFlag_hint.js

--- plugins/game-guessFlag_hint.js
+++ plugins/game-guessFlag_hint.js
@@ -1,12 +1,17 @@
 let handler = async (m, { conn }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-    let id = m.chat
-    if (!(id in conn.tebakbendera)) throw false
-    let json = conn.tebakbendera[id][1]
-    conn.reply(m.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
-}
-handler.command = /^fhint$/i
+    conn.tebakbendera = conn.tebakbendera || {};
+    let id = m.chat;
 
+    if (!(id in conn.tebakbendera)) throw false;
 
+    let json = conn.tebakbendera[id][1];
+    let hint = json.name.replace(/[AIUEOaiueo]/g, '_'); // Replace vowels with underscores
 
-export default handler
+    conn.reply(m.chat, `🤖 *Here's your hint, weakling:*  
+\`\`\`${hint}\`\`\`  
+💡 *Good luck, if that's even possible for you.*`, m);
+};
+
+handler.command = /^fhint$/i;
+
+export default handler;
