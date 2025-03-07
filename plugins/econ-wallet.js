587aaaed4e278bbc4f551b1f9e72ba4813c3e084
KashifCodeMaster <156572838+KashifCodeMaster@users.noreply.github.com>
2025-03-07 23:37:44 +0500

    Update econ-wallet.js

--- plugins/econ-wallet.js
+++ plugins/econ-wallet.js
@@ -1,17 +1,17 @@
-let handler = async (m, {conn, usedPrefix}) => {
+let handler = async (m, { conn, usedPrefix }) => {
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid?.[0] || m.fromMe ? conn.user.jid : m.sender;
+    if (!(who in global.db.data.users)) throw `🟨 The user is not found in my database`;
 
-	let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let user = global.db.data.users[who]
+    let user = global.db.data.users[who];
+    let username = conn.getName(who);
+    
+    let message = `👛 *Wallet | ${username}*\n\n🪙 *Gold* : ${user.credit}`;
+    
+    conn.reply(m.chat, message, m, { mentions: [who], quoted: m });
+};
 
-    let username = conn.getName(who)
-    if (!(who in global.db.data.users)) throw `🟨 The user is not found in my database`
-    conn.reply(m.chat, `👛 *Wallet | ${username}*
+handler.help = ['wallet'];
+handler.tags = ['economy'];
+handler.command = ['wallet', 'gold'];
 
-🪙 *Gold* : ${user.credit}
-`, m, { mentions: [who] })
-}
-handler.help = ['wallet']
-handler.tags = ['economy']
-handler.command = ['wallet', 'gold'] 
-
-export default handler
+export default handler;
